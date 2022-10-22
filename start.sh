61d8de1486752370f54e167975f4346db990270e
enthada-myre <111719488+enthada-myre@users.noreply.github.com>
2022-10-22 14:34:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/enthada-myre/Adv-Auto-Filter-Bot-V2 /Adv-Auto-Filter-Bot-V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Adv-Auto-Filter-Bot-V2
 fi
-cd /EvaMaria
+cd /Adv-Auto-Filter-Bot-V2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
